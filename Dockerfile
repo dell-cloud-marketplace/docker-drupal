e8ba244267aa67b11a801750013213f162d1ef9d
emilie <emilie.poccioli@gmail.com>
2014-10-28 14:54:50 +0000

    Made Drupal LAMP-based

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ ADD ./start.sh /start.sh
 ADD ./foreground.sh /etc/apache2/foreground.sh
 ADD ./supervisord.conf /etc/supervisord.conf
 
+#Replace /var/www/html to /var/www/
+RUN sed -i "s/\/var\/www\/html/\/var\/www/g" /etc/apache2/sites-available/000-default.conf
 
 # Retrieve drupal
 RUN rm -rf /var/www/ ; cd /var ; drush dl drupal ; mv /var/drupal*/ /var/www/
--- start.sh
+++ start.sh
@@ -29,7 +29,8 @@ if [ ! -f /var/www/sites/default/settings.php ]; then
 	echo $DRUPAL_PASSWORD > /drupal-db-pw.txt
 	mysqladmin -u root password $MYSQL_PASSWORD 
 	mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE drupal; GRANT ALL PRIVILEGES ON drupal.* TO 'drupal'@'localhost' IDENTIFIED BY '$DRUPAL_PASSWORD'; FLUSH PRIVILEGES;"
-	sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/sites-available/000-default.conf
+	sed -i 's/AllowOverride None/AllowOverride All/g' /etc/apache2/sites-available/000-default.conf
+	 sed -i 's/AllowOverride Limit/AllowOverride All/g' /etc/apache2/sites-available/000-default.conf
 	a2enmod rewrite vhost_alias
 	cd /var/www/
 	drush site-install standard -y --account-name=admin --account-pass=admin --db-url="mysqli://drupal:${DRUPAL_PASSWORD}@localhost:3306/drupal"
