a748282ac2a8118bd2f3e490c79652b0782c9e7a
Ubuntu <ubuntu@ip-10-91-173-226.eu-west-1.compute.internal>
2014-10-28 20:30:12 +0000

    Formatting

--- start.sh
+++ start.sh
@@ -2,39 +2,58 @@
 
 VOLUME_HOME="/var/lib/mysql"
 
-sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
-	-e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
+# Possibly invoke the inherited script to create the admin user.
 if [[ ! -d $VOLUME_HOME/mysql ]]; then
-	echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
-	echo "=> Installing MySQL ..."
-	mysql_install_db > /dev/null 2>&1
-	echo "=> Done!"
-	/create_mysql_admin_user.sh
+    echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
+    echo "=> Installing MySQL ..."
+    mysql_install_db > /dev/null 2>&1
+    echo "=> Done!"
+    /create_mysql_admin_user.sh
 else
-	echo "=> Using an existing volume of MySQL"
+    echo "=> Using an existing volume of MySQL"
 fi
 
 if [ ! -f /var/www/sites/default/settings.php ]; then
-	# Start mysql
-	/usr/bin/mysqld_safe & 
-	sleep 10s
-	# Generate random passwords 
-	DRUPAL_DB="drupal"
-	MYSQL_PASSWORD=`pwgen -c -n -1 12`
-	DRUPAL_PASSWORD=`pwgen -c -n -1 12`
-	# This is so the passwords show up in logs. 
-	echo mysql root password: $MYSQL_PASSWORD
-	echo drupal password: $DRUPAL_PASSWORD
-	echo $MYSQL_PASSWORD > /mysql-root-pw.txt
-	echo $DRUPAL_PASSWORD > /drupal-db-pw.txt
-	mysqladmin -u root password $MYSQL_PASSWORD 
-	mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE drupal; GRANT ALL PRIVILEGES ON drupal.* TO 'drupal'@'localhost' IDENTIFIED BY '$DRUPAL_PASSWORD'; FLUSH PRIVILEGES;"
-	sed -i 's/AllowOverride None/AllowOverride All/g' /etc/apache2/sites-available/000-default.conf
-	sed -i 's/AllowOverride Limit/AllowOverride All/g' /etc/apache2/sites-available/000-default.conf
-	a2enmod rewrite vhost_alias
-	cd /var/www/
-	drush site-install standard -y --account-name=admin --account-pass=admin --db-url="mysqli://drupal:${DRUPAL_PASSWORD}@localhost:3306/drupal"
-	killall mysqld
-	sleep 10s
+
+    # Start mysql
+    /usr/bin/mysqld_safe & 
+    sleep 10s
+
+    DRUPAL_DB="drupal"
+
+    # Generate random passwords
+    MYSQL_PASSWORD=`pwgen -c -n -1 12`
+    DRUPAL_PASSWORD=`pwgen -c -n -1 12`
+
+    # The passwords will show up in the logs. 
+    echo "MySQL root user password:" $MYSQL_PASSWORD
+    echo "MySQL drupal user password:" $DRUPAL_PASSWORD
+    echo $MYSQL_PASSWORD > /mysql-root-pw.txt
+    echo $DRUPAL_PASSWORD > /drupal-db-pw.txt
+
+    # Set the root user password
+    mysqladmin -u root password $MYSQL_PASSWORD 
+
+    # Create the database
+    mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE drupal; \
+            GRANT ALL PRIVILEGES ON drupal.* TO 'drupal'@'localhost' \
+            IDENTIFIED BY '$DRUPAL_PASSWORD'; FLUSH PRIVILEGES;"
+
+    # Update permissions.
+    sed -i 's/AllowOverride None/AllowOverride All/g' \
+            /etc/apache2/sites-available/000-default.conf
+    sed -i 's/AllowOverride Limit/AllowOverride All/g' \
+            /etc/apache2/sites-available/000-default.conf
+
+    a2enmod rewrite vhost_alias
+
+    # Install Drupal
+    cd /var/www/
+    drush site-install standard -y --account-name=admin --account-pass=admin \
+            --db-url="mysqli://drupal:${DRUPAL_PASSWORD}@localhost:3306/drupal"
+    
+    killall mysqld
+    sleep 10s
 fi
+
 supervisord -n
